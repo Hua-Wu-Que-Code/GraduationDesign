8b6e3d9113cee38ae831ed70b69db7ffcab310a3
花无缺 <1178306549@qq.com>
2023-02-27 00:44:40 +0800

    添加了后台界面，有可能会进行更换，在前端文件中添加axios，Vant插件

--- bac-view/src/main.js
+++ bac-view/src/main.js
@@ -6,13 +6,8 @@ import network from "./network/index";
 Vue.prototype.$ajax = network;
 Vue.config.productionTip = false
 
-import Vant from 'vant';
-import 'vant/lib/index.css';
-
-Vue.use(Vant);
-
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: function (h) { return h(App) }
 }).$mount('#app')
